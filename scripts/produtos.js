5acb4bb98b7cec719be07b9494c8327069400d97
Paulo Roberto <devpaulorag@gmail.com>
2025-01-12 11:13:54 -0300

    adds `body.css` file and separates CSS responsibilities

--- scripts/produtos.js
+++ scripts/produtos.js
@@ -1,64 +1,71 @@
 let listaProdutos = [
-  {
-    id: 1,
-    name: 'Bomba',
-    img: 'img/bomba.png',
-    price: 8.00,
-    description: 'Pão, hambúrguer, salsicha, presunto, queijo, ovo, alface e tomate'
-  },
-  {
-    id: 2,
-    name: 'X-Tudo',
-    img: 'img/x-tudo.png',
-    price: 17.00,
-    description: 'Pão, hambúrguer, presunto, queijo, ovo, batata-palha, milho, bacon'
-  },
-  {
-    id: 3,
-    name: 'X-Tudo de frango',
-    img: 'img/x-tudo-frango.png',
-    price: 19.00,
-    description: 'Pão, peito de frango, presunto, queijo, ovo, batata-palha, milho, bacon'
-  },
-  {
-    id: 4,
-    name: 'X-Brocado',
-    img: 'img/x-brocado.png',
-    price: 25.00,
-    description: 'Pão, 2 hambúrgueres, presunto, queijo, bacon, milho, alface, tomate, salsicha'
-  }, {
-    id: 5,
-    name: 'Bata-Frita',
-    img: 'img/bata-frita.png',
-    price: 10.00,
-    description: 'Porção de Bata-frita simples'
-  },
-  {
-    id: 6,
-    name: 'Bata-Frita com Bacon',
-    img: 'img/bata-frita-bacon.png',
-    price: 15.00,
-    description: 'Porção de Bata-frita e bacon'
-  },
-  {
-    id: 7,
-    name: 'Cervejas',
-    img: 'img/cervejas.png',
-    price: 8.00,
-    description: 'Heineken, Budweiser, Smirnoff, Brahma, Skol, Eisenbahn, Stella Artois'
-  },
-  {
-    id: 8,
-    name: 'Refrigerante lata',
-    img: 'img/refrigerante.png',
-    price: 5.00,
-    description: 'Coca-Cola, Coca-Cola Zero, Kuat, Fanta Laranja, Schweppes'
-  },
-  {
-    id: 9,
-    name: 'Sucos naturais',
-    img: 'img/sucos.png',
-    price: 6.00,
-    description: 'Maracujá, Laranja e Limão'
-  }
+    {
+        id: 1,
+        name: "Bomba",
+        img: "/assets/image/bomba.png",
+        price: 8.0,
+        description:
+            "Pão, hambúrguer, salsicha, presunto, queijo, ovo, alface e tomate",
+    },
+    {
+        id: 2,
+        name: "X-Tudo",
+        img: "/assets/image/x-tudo.png",
+        price: 17.0,
+        description:
+            "Pão, hambúrguer, presunto, queijo, ovo, batata-palha, milho, bacon",
+    },
+    {
+        id: 3,
+        name: "X-Tudo de frango",
+        img: "/assets/image/x-tudo-frango.png",
+        price: 19.0,
+        description:
+            "Pão, peito de frango, presunto, queijo, ovo, batata-palha, milho, bacon",
+    },
+    {
+        id: 4,
+        name: "X-Brocado",
+        img: "/assets/image/x-brocado.png",
+        price: 25.0,
+        description:
+            "Pão, 2 hambúrgueres, presunto, queijo, bacon, milho, alface, tomate, salsicha",
+    },
+    {
+        id: 5,
+        name: "Bata-Frita",
+        img: "/assets/image/bata-frita.png",
+        price: 10.0,
+        description: "Porção de Bata-frita simples",
+    },
+    {
+        id: 6,
+        name: "Bata-Frita com Bacon",
+        img: "/assets/image/bata-frita-bacon.png",
+        price: 15.0,
+        description: "Porção de Bata-frita e bacon",
+    },
+    {
+        id: 7,
+        name: "Cervejas",
+        img: "/assets/image/cervejas.png",
+        price: 8.0,
+        description:
+            "Heineken, Budweiser, Smirnoff, Brahma, Skol, Eisenbahn, Stella Artois",
+    },
+    {
+        id: 8,
+        name: "Refrigerante lata",
+        img: "/assets/image/refrigerante.png",
+        price: 5.0,
+        description:
+            "Coca-Cola, Coca-Cola Zero, Kuat, Fanta Laranja, Schweppes",
+    },
+    {
+        id: 9,
+        name: "Sucos naturais",
+        img: "/assets/image/sucos.png",
+        price: 6.0,
+        description: "Maracujá, Laranja e Limão",
+    },
 ];
